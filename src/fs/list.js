a225d1ceda44efd5bf3e008d099a70dc5b41fcff
vlaru <dn210690nvs@gmail.com>
2025-10-22 23:43:15 +0200

    feat: implement list files functionality in the directory

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const folderName = 'files';
+const errorMsg = 'FS operation failed';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-  // Write your code here
+  const src = path.join(__dirname, folderName);
+  try {
+    const files = await fs.readdir(src);
+    console.log(files);
+  } catch (error) {
+    throw new Error(errorMsg);
+  }
 };
 
 await list();
