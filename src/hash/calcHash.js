083475e37d80a2cec693c28f40e5e2ceafb88855
vlaru <dn210690nvs@gmail.com>
2025-10-23 21:28:14 +0200

    feat: implement file hash calculation functionality

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,35 @@
+import crypto from 'node:crypto';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const folderName = 'files';
+const fileName = 'fileToCalculateHashFor.txt';
+const errorMsg = 'FS operation failed';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-  // Write your code here
+  const filePath = path.join(__dirname, folderName, fileName);
+  try {
+    await fs.promises.access(filePath);
+    const hash = crypto.createHash('sha256')
+    const stream = fs.createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+      console.log(hash.digest('hex'));
+    });
+
+    stream.on('error', () => {
+      throw new Error(errorMsg);
+    });
+  } catch (error) {
+    throw new Error(errorMsg);
+  }
 };
 
 await calculateHash();
