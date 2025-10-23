7972422a181eaaf36b5ad2f3417fe8bed5001f8d
vlaru <dn210690nvs@gmail.com>
2025-10-23 16:42:48 +0200

    feat: implement environment variable parsing and logging

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-  // Write your code here
+  const parse = Object.entries(process.env)
+  .filter(([key]) => key.startsWith('RSS_'))
+  .map(([key, value]) => `${key}=${value}`).join('; ');
+  console.log(parse);
 };
 
 parseEnv();
