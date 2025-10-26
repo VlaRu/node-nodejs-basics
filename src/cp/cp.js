323ad5256514b440ff19beb66262dae375f45014
vlaru <dn210690nvs@gmail.com>
2025-10-26 22:40:08 +0100

    feat: implement child process spawning

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import {fork} from 'child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const folderName = 'files';
+const fileName = 'script.js';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const src = path.join(__dirname, folderName, fileName);
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  fork(src,args)
 };
 
 // Put your arguments in function call to test this functionality
