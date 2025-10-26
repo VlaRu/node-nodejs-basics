cb703e36013cb68c3beac8317972a0be40fa9021
vlaru <dn210690nvs@gmail.com>
2025-10-26 18:25:37 +0100

    feat: implement chunk reversing transformation in stream pipeline

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { pipeline } from 'node:stream/promises';
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
+const chunkReverse = new Transform({
+  transform(chunk, encoding, callback) {
+    const s = chunk.toString().split('').reverse().join('');
+    callback(null, s);
+  }
+});
+
 const transform = async () => {
-  // Write your code here
+  try {
+    await pipeline(stdin, chunkReverse, stdout, { end: false });
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 };
 
 await transform();
